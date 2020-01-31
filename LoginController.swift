b285a632a137f1d6939c8ea488c010e34fffaaf3
Lama Alherbish <49396756+AlherbishLama@users.noreply.github.com>
2020-01-31 16:44:08 +0300

    Add files via upload

---
+++ LoginController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginController.swift
+//  Home
+//
+//  Created by Lama Alherbish on 1/30/20.
+//  Copyright © 2020 Lama Alherbish. All rights reserved.
+//
+
+import UIKit
+
+class LoginController:UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
